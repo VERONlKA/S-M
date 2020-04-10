096a4375d569613d88c44fd20b414f40cd8398b7
byeHana <61992723+byeHana@users.noreply.github.com>
2020-04-10 18:29:17 +0200

    difficulty level menu added

---
+++ Snake/core/src/com/snake/game/state/DifficultyLevel.java
@@ -0,0 +1,50 @@
+package com.snake.game.state;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.snake.game.SnakeGame;
+
+public class DifficultyLevel extends State{
+
+    private Texture background;
+    private Texture easy;
+    private Texture medium;
+    private Texture hard;
+    private Texture back;
+
+    public DifficultyLevel(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("forestBG.jpg");
+        easy = new Texture("easy.PNG");
+        medium = new Texture("medium.PNG");
+        hard = new Texture("hard.PNG");
+        back = new Texture("return.PNG");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0,0, SnakeGame.WIDTH, SnakeGame.HEGHT);
+        sb.draw(back, 79,700, 60,60);
+        sb.draw(easy, 70,350, 330, 330);
+        sb.draw(medium, 70, 210, 330,330);
+        sb.draw(hard, 70, 70, 330,330);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
