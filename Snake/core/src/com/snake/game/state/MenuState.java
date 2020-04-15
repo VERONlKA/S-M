03d824a37b92ea881f5bd8759a1781e502f18bd9
byeHana <61992723+byeHana@users.noreply.github.com>
2020-04-15 22:58:14 +0300

    new start screen (menu screen) + button can be touched

--- Snake/core/src/com/snake/game/state/MenuState.java
+++ Snake/core/src/com/snake/game/state/MenuState.java
@@ -1,33 +1,54 @@
 package com.snake.game.state;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.snake.game.SnakeGame;
 
 public class MenuState extends State {
     private Texture backgroundMenu;
-    private Texture playBtn;
     private Texture logoSnake;
+
     private Texture settingsBtn;
+
+    private Texture playBtn;
+    private Sprite playSprite;
+    private SpriteBatch batch;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
         backgroundMenu = new Texture("menuBackGR.jpg");
-        playBtn = new Texture("playButton.png");
         logoSnake = new Texture("logoSnake.PNG");
         settingsBtn = new Texture("settingsButton.PNG");
         camera.setToOrtho(false, SnakeGame.WIDTH , SnakeGame.HEGHT);
+        create();
 
     }
 
     @Override
     protected void handleInput() {
 
+        if (Gdx.input.isTouched()) {
+            if (playSprite.getBoundingRectangle().contains(Gdx.input.getX(), Gdx.graphics.getHeight()-Gdx.input.getY())) {
+                gsm.push(new ChooseAnAction(gsm));
+                dispose();
+            }
+        }
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
 
+    }
+
+    public void create(){
+        playBtn = new Texture("playButton.png");
+        batch = new SpriteBatch();
+        playSprite=new Sprite(playBtn, 0,0, 100,100);
+        playSprite.setSize(105,105);
+        playSprite.setPosition(190, 140);
     }
 
     @Override
@@ -35,12 +56,15 @@ public class MenuState extends State {
         sb.setProjectionMatrix(camera.combined);
         sb.begin();
         sb.draw(backgroundMenu, 0,0, SnakeGame.WIDTH, SnakeGame.HEGHT);
-        sb.draw(playBtn, 190, 120, 100,100  );
-        sb.draw(logoSnake, 60, 260, 370,370);
+        sb.draw(playBtn, 190, 140, 105,105  );
+        sb.draw(logoSnake, 95, 245, 290,290);
         sb.draw(settingsBtn, 370,700, 45,45);
 
-        sb.end();
+        batch.begin();
+        playSprite.draw(batch);
+        batch.end();
 
+        sb.end();
 
     }
 
